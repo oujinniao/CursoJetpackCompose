bc6f3d2e69b33126d3b0157d46af5bbc6eccec24
lenovo <grajales1725@gmail.com>
2025-08-02 19:01:56 -0400

    version de JPC aplicando la función derivedStateOf, ejemplo 2

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -7,17 +7,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
 
@@ -33,12 +41,52 @@ class MainActivity : ComponentActivity() {
                 //rememberMutableStateOfExample()
                 //MyScreen()
                 //MyScreen2()
-                CounterScreen()
+                //CounterScreen()
+                MyDerivedState()
 
             }
         }
     }
 }
+
+@Composable
+fun MyDerivedState(){
+    var text by rememberSaveable { mutableStateOf("") }
+
+    val textColor by remember {
+        derivedStateOf {
+            if (text.length>10) Color.Red else  Color.Magenta
+
+
+            }
+
+        }
+    Column(
+        modifier= Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement= Arrangement.Center,
+        horizontalAlignment= Alignment.CenterHorizontally
+
+    ){
+    OutlinedTextField(
+        value=text,
+        onValueChange = {text=it},
+        label={Text(text="Escribe algo")},
+    )
+        Spacer(modifier=Modifier
+
+            .height(16.dp))
+
+        Text(
+            text=" Longitud: ${text.length}",
+            color=textColor,
+            style=MaterialTheme.typography.headlineMedium
+        )
+    }
+
+}
+
 @Composable
 fun CounterScreen() {
 
