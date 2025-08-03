e7cbc593d9dfcedae57809124bfe9a86c027a3ce
lenovo <grajales1725@gmail.com>
2025-08-02 20:17:27 -0400

    version de JPC aplicando la función mutableStateListOf

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -7,25 +7,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
 
@@ -42,7 +45,8 @@ class MainActivity : ComponentActivity() {
                 //MyScreen()
                 //MyScreen2()
                 //CounterScreen()
-                MyDerivedState()
+                //MyDerivedState()
+                MyMutableStateListOf()
 
             }
         }
@@ -50,63 +54,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyDerivedState(){
-    var text by rememberSaveable { mutableStateOf("") }
-
-    val textColor by remember {
-        derivedStateOf {
-            if (text.length>10) Color.Red else  Color.Magenta
-
-
-            }
-
-        }
-    Column(
-        modifier= Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement= Arrangement.Center,
-        horizontalAlignment= Alignment.CenterHorizontally
-
-    ){
-    OutlinedTextField(
-        value=text,
-        onValueChange = {text=it},
-        label={Text(text="Escribe algo")},
-    )
-        Spacer(modifier=Modifier
-
-            .height(16.dp))
-
-        Text(
-            text=" Longitud: ${text.length}",
-            color=textColor,
-            style=MaterialTheme.typography.headlineMedium
+fun MyMutableStateListOf() {
+    val itemsList = remember {
+        mutableStateListOf(
+            "Item 1", "Item 2", "Item 3"
         )
     }
 
-}
-
-@Composable
-fun CounterScreen() {
-
-    var counter = remember { mutableIntStateOf(0) }
-    val esPar by remember { derivedStateOf { counter.value % 2 == 0 } }
-
     Column(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Contador: ${counter.value}")
-        Text(text = if (esPar) "Es par" else "Es impar")
 
+    ) {
         Button(
-            onClick = { counter.value++ }
+            onClick = { itemsList.add("New Item ${itemsList.size + 1}") }
+
         ) {
-            Text(text = "Incrementar")
+
+            Text(text = "Add Item")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(itemsList) { item ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+
+                        ) {
+
+                        Text(item,
+                            style = MaterialTheme.typography.bodyLarge)
+
+                        IconButton(
+                            onClick = { itemsList.remove(item) }
+                        ) {
+                            Icon(Icons.Default.Delete,
+                                contentDescription = "Delete")
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
