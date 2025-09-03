8dc72ba886357c530f5a05053744b8e26febd21d
lenovo <grajales1725@gmail.com>
2025-09-03 18:50:26 -0400

    version de JPC usando LaunchedEffect

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.cursodejetpackcompose
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +23,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
 import kotlinx.coroutines.delay
 
@@ -31,58 +40,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CursoDeJetpackComposeTheme {
-            DatosUsuarioScreen()
+                //MyScreen()
+                CounterLaunchedEffect()
+
 
             }
         }
     }
 
-    suspend fun obtenerDatosAPI(): String { //suspend fun se ejecuta dentro de una corrutina(corutina se ejecuta de manera asincrona)
-        delay(5000)
-        return "Hola"
-    }
     @Composable
-    fun DatosUsuarioScreen() {
-        var usuario by remember{ mutableStateOf<String?>(null)}
+    fun CounterLaunchedEffect() {
+        var counter by remember { mutableStateOf(0) }
 
-        var isloading by remember{ mutableStateOf(true)}
-
-
-
-        LaunchedEffect(Unit) {
-            usuario = obtenerDatosAPI()
-            isloading = false
-
+        LaunchedEffect(key1 = counter) {
+            Log.d("CounterLaunchedEffect", "Counter: $counter")
+            delay(3000)
+            counter++
         }
-        Column (
-            modifier= Modifier
+        Column(
+            modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Counter: $counter",
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 24.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                textDecoration = TextDecoration.Underline,
+                lineHeight = 32.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        ){
-            if(isloading){
-                CircularProgressIndicator(
-                    color = MaterialTheme.colorScheme.primary
-
-                )
-            }else{
-                usuario?.let {
-                    Text(
-                        text ="En hora buena: $it",
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = MaterialTheme.colorScheme.primary,
-
-                        )
-                }
+            Button(onClick = { counter++ }) {
             }
+            Text(text = "Incrementar")
         }
-
     }
 }
 
 
+            @Composable
+            fun MyScreen() {
+                //se ejecuta una sola vez con Unit
+                LaunchedEffect(Unit) {
+                    //courutineScope
+                    delay(3000)
+                    Log.d("MyScreen", "3 segundos han pasado")
+
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "Hola Jetpack Compose")
+                }
+            }
 
 
 
