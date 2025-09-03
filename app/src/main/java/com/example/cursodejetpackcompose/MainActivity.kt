f09492dc8fd85973a2ffc3cb10fd46298987e1c0
lenovo <grajales1725@gmail.com>
2025-09-03 19:30:27 -0400

    version de JPC usando CoroutineScope

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.cursodejetpackcompose
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,22 +15,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -41,72 +35,49 @@ class MainActivity : ComponentActivity() {
         setContent {
             CursoDeJetpackComposeTheme {
                 //MyScreen()
-                CounterLaunchedEffect()
+                //CounterLaunchedEffect()
+                RememberCoroutineScope()
 
 
             }
         }
     }
-
     @Composable
-    fun CounterLaunchedEffect() {
-        var counter by remember { mutableStateOf(0) }
+    fun RememberCoroutineScope() {
+        val scope = rememberCoroutineScope()
 
-        LaunchedEffect(key1 = counter) {
-            Log.d("CounterLaunchedEffect", "Counter: $counter")
-            delay(3000)
-            counter++
-        }
-        Column(
-            modifier = Modifier
+        var text by remember {mutableStateOf("Picha aquí")}
+
+            Column(
+                modifier= Modifier
                 .fillMaxSize()
                 .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Counter: $counter",
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.primary,
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.SansSerif,
-                fontSize = 24.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                textDecoration = TextDecoration.Underline,
-                lineHeight = 32.sp
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(onClick = { counter++ }) {
-            }
-            Text(text = "Incrementar")
-        }
-    }
-}
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.headlineMedium)
 
 
-            @Composable
-            fun MyScreen() {
-                //se ejecuta una sola vez con Unit
-                LaunchedEffect(Unit) {
-                    //courutineScope
-                    delay(3000)
-                    Log.d("MyScreen", "3 segundos han pasado")
+                Spacer(modifier = Modifier.height(16.dp))
 
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                Button(
+                    onClick = {
+                        scope.launch {
+                            text = "Presionado"
+                            delay(3000)
+                            text = "FIN TAREA"
+                        }
+                    }
                 ) {
-                    Text(text = "Hola Jetpack Compose")
+                    Text(text = "Ejecutar tarea")
                 }
             }
 
+
+            }
+
+    }
 
 
