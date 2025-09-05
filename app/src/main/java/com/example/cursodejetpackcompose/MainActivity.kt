5fc22e9ae0604f3e6dab74583b5ca83802b6d3ce
lenovo <grajales1725@gmail.com>
2025-09-05 19:05:05 -0400

    version de JPC usando la funcion DisposableEffect

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -6,22 +6,15 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
@@ -33,7 +26,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CursoDeJetpackComposeTheme {
-                SideEffectExample()
+               DisposableEffectExample()
 
 
             }
@@ -42,61 +35,30 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun SideEffectExample() {
-
-        var items by rememberSaveable {
-            mutableStateOf(listOf("Item 1", "Item 2", "Item 3"))
-        }
-
-        SideEffect {
-            Log.d("Recomposición", "Recomposición de la pantalla CON  ${items.size} elementos")
-        }
+    fun DisposableEffectExample() {
         Column(
-            modifier = Modifier
+            modifier= Modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            DisposableEffect(key1 = Unit){
+             Log.d("DisposableEffect", "Comenzando el DisposableEffect")
 
+               onDispose {
+                   Log.d("DisposableEffect", "Deteniendo el DisposableEffect")
+               }
 
-        ) {
-            Text(
-                text = "Lista de elementos",
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-            LazyColumn(
-                modifier = Modifier.fillMaxSize()
-
-            ) {
-                items(items) { item ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
-
-                    ) {
-
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(8.dp),
-                            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
-                        ) {
-                            Text(
-                                text = item,
-                                style = MaterialTheme.typography.bodyLarge,
-                                modifier = Modifier.padding(16.dp)
-
-                            )   }
-
-
-                            }
-
-                    }
                 }
-
-
+            Text(
+                text = "Componente con DisposableEffect",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(bottom = 16.dp),
+                )
+            }
             }
         }
-    }
+
+
 
