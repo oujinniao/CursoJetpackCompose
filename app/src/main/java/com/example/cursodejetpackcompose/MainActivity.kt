6e519650b1e60d4990562979a78049e4e2345194
lenovo <grajales1725@gmail.com>
2025-09-05 11:08:47 -0400

    version de JPC usando Funciones suspendidas y Courrutinas

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -2,24 +2,18 @@ package com.example.cursodejetpackcompose
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -36,48 +30,43 @@ class MainActivity : ComponentActivity() {
             CursoDeJetpackComposeTheme {
                 //MyScreen()
                 //CounterLaunchedEffect()
-                RememberCoroutineScope()
+                //CounterRememberUpdatedState()
+                CoroutineScopeExample()
 
 
             }
         }
+
+        }
+
+    suspend fun slowTask(){
+        Log.d("MainActivity", "Slow task started")
+        delay(5000)
+        Log.d("MainActivity", "Slow task finished")
+
     }
     @Composable
-    fun RememberCoroutineScope() {
+    fun CoroutineScopeExample(){
+
         val scope = rememberCoroutineScope()
 
-        var text by remember {mutableStateOf("Picha aquí")}
-
-            Column(
-                modifier= Modifier
+        Column(
+            modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = text,
-                    style = MaterialTheme.typography.headlineMedium)
+            .padding(16.dp),
 
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
 
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(
-                    onClick = {
-                        scope.launch {
-                            text = "Presionado"
-                            delay(3000)
-                            text = "FIN TAREA"
-                        }
-                    }
-                ) {
-                    Text(text = "Ejecutar tarea")
+        ){
+            Button(onClick = {
+                scope.launch {
+                    slowTask()
                 }
+            }) {
+                Text(text = "Start Slow Task")
             }
-
-
-            }
-
+        }
     }
-
+}
 
