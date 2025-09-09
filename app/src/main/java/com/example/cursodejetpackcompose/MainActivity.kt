a80d788181fde6534e6fe4d0fb70ad9bebe5767f
lenovo <grajales1725@gmail.com>
2025-09-09 11:58:51 -0300

    version de JPC usando la funcion CheckBox

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -6,27 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
 
@@ -36,15 +36,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CursoDeJetpackComposeTheme {
-                OutlinedTextFieldExample()
+                PreferenceApp()
             }
         }
     }
 
     @Composable
-    fun OutlinedTextFieldExample() {
-        var text by remember { mutableStateOf("") }
-        val isError = text.length > 10
+    fun CheckBoxExample() {
+        var checked by remember { mutableStateOf(false) }
 
         Column(
             modifier = Modifier
@@ -52,64 +51,113 @@ class MainActivity : ComponentActivity() {
                 .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text("Nombre") },
-                placeholder = { Text("Escribe algo") },
-                leadingIcon = { // ✅ Este es el nombre de parámetro correcto.
-                    Icon(
-                        imageVector = Icons.Default.Warning,
-                        contentDescription = "Advertencia"
-                    )
-                },
-                trailingIcon = {
-                    if (isError) {
-                        Icon(
-                            Icons.Default.Warning,
-                            "Error",
-                            tint = MaterialTheme.colorScheme.error
-                        )
-                    }
-                },
-                isError = isError,
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    imeAction = ImeAction.Done
-                ),
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 16.dp),
-                shape = RoundedCornerShape(8.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = MaterialTheme.colorScheme.primary,
-                    unfocusedBorderColor = MaterialTheme.colorScheme.secondary,
-                    errorBorderColor = MaterialTheme.colorScheme.error,
-                    focusedLabelColor = MaterialTheme.colorScheme.primary,
-                    unfocusedLabelColor = MaterialTheme.colorScheme.secondary,
-                    errorLabelColor = MaterialTheme.colorScheme.error,
-                    focusedTrailingIconColor = MaterialTheme.colorScheme.primary,
-                    unfocusedTrailingIconColor = MaterialTheme.colorScheme.secondary,
-                    errorTrailingIconColor = MaterialTheme.colorScheme.error,
-                    focusedLeadingIconColor = MaterialTheme.colorScheme.primary,
-                    unfocusedLeadingIconColor = MaterialTheme.colorScheme.secondary,
-                    errorLeadingIconColor = MaterialTheme.colorScheme.error,
-                    focusedTextColor = MaterialTheme.colorScheme.onSurface,
-                    unfocusedTextColor = MaterialTheme.colorScheme.onSurface,
-                    cursorColor = MaterialTheme.colorScheme.primary
-                )
-            )
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Checkbox(
+                    checked = checked,
+                    onCheckedChange = { checked = it },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = MaterialTheme.colorScheme.primary,
+                        uncheckedColor = MaterialTheme.colorScheme.onSurface,
+                        checkmarkColor = MaterialTheme.colorScheme.surface,
 
-            if (isError) {
+                        )
+                )
+                Spacer(modifier = Modifier.padding(2.dp))
+
                 Text(
-                    text = "El texto no puede tener más de 10 caracteres",
-                    color = MaterialTheme.colorScheme.error,
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.padding(start = 16.dp, top = 4.dp)
+                    text =
+                        if (checked) "Checkbox marcado"
+                        else "Checkbox no marcado"
                 )
             }
+
+
+        }
+
+    }
+
+    @Composable
+    fun PreferenceApp() {
+        val options = listOf("Opción 1", "Opción 2", "Opción 3", "Opción 4", "Opción 5")
+
+        val stateOption = remember {
+            mutableStateMapOf<String, Boolean>().apply {
+                options.forEach {
+                    put(it, false)
+                }
+            }
+
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Selecciona una opción",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(bottom = 16.dp),
+
+                )
+
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            LazyColumn {
+                items(options) { option ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Checkbox(
+                            checked = stateOption[option] ?: false,
+                            onCheckedChange = {
+                                stateOption[option] = it
+                            },
+                            colors = CheckboxDefaults.colors(
+                                checkedColor = MaterialTheme.colorScheme.primary,
+                                uncheckedColor = MaterialTheme.colorScheme.onSurface,
+                                checkmarkColor = MaterialTheme.colorScheme.surface
+                            )
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text(
+                            text = option,
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
+
+                }
+
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+
+            val selectedOptions = stateOption.filterValues { it }.keys
+            val selectedText = if (selectedOptions.isEmpty()) {
+                "Ninguna"
+            } else {
+                selectedOptions.joinToString(", ")
+            }
+
+            Text(
+                text = "Opciones seleccionadas: $selectedText",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
         }
     }
 }
