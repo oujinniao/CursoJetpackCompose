5b45555568f1d9253dc08732df2651f1f3bcfef5
lenovo <grajales1725@gmail.com>
2025-09-11 15:13:43 -0300

    version de JPC usando la funcion Switch

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -12,17 +12,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
 
@@ -32,76 +38,84 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CursoDeJetpackComposeTheme {
-                RadioButtonExample()
 
+            SwitchExample()
             }
         }
     }
 
     @Composable
+    fun SwitchExample(){
+        var wifiEnable by remember { mutableStateOf(false) }
 
-    fun RadioButtonExample() {
+        Column (
+            modifier=Modifier.fillMaxSize()
+            .padding(16.dp),
 
-        val options = listOf("Opción 1", "Opción 2", "Opción 3")
-
-        var selectedOption = remember { mutableStateOf(options[0]) }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                    horizontalAlignment = Alignment.CenterHorizontally
 
         ){
-            Text(
-                text = "Selecciona una opción:",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(bottom = 16.dp)
-
-            )
-            LazyColumn(
-                modifier = Modifier
+            Row (
+                modifier= Modifier
                     .fillMaxWidth()
-            ) {
-                items(options){option ->
+                .padding(horizontal = 16.dp, vertical = 12.dp),
 
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 8.dp),
-                        verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+
+
+            ){
+                Column (
+                    modifier=Modifier.weight(1f)
+
+                ){
+                    Text(
+                        text="Wifi",
+                        style= MaterialTheme.typography.titleMedium,
+                    )
+        Spacer(modifier=Modifier.height(8.dp))
+                    Text(
+                        text= if (wifiEnable)"Conectado a la red" else "Desconectado de la red",
+                        style= MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
 
                     )
-                    {
+                Switch(
+                    checked = wifiEnable,
+                    onCheckedChange = {wifiEnable=it},
+                    thumbContent = {
+                        if (wifiEnable){
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Activado",
+                               modifier = Modifier.size(SwitchDefaults.IconSize),
+                                tint = Color.Green
 
-                        RadioButton(
-                            selected = (option == selectedOption.value), // Corrección
-                            onClick = { selectedOption.value = option }, // Corrección
-                            colors = RadioButtonDefaults.colors(
-                                selectedColor = MaterialTheme.colorScheme.primary,
-                                unselectedColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                                disabledSelectedColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.38f),
-                                disabledUnselectedColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
                             )
-                        )
-                        Text(
-                            text = option,
-                            style = MaterialTheme.typography.bodyLarge,
-                            modifier = Modifier.padding(start = 16.dp)
+                        }else{
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                contentDescription = "Desactivado",
+                                modifier = Modifier.size(SwitchDefaults.IconSize),
+                                tint = Color.Red.copy(alpha = 0.6f)
                             )
-                    }
-
+                        }
+                        },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = MaterialTheme.colorScheme.primary,
+                        checkedTrackColor = Color.Green.copy(alpha = 0.6f),
+                        uncheckedThumbColor = MaterialTheme.colorScheme.surfaceVariant,
+                        uncheckedTrackColor = Color.Red.copy(alpha = 0.6f)
+                    )
+                )
 
                 }
             }
-            Spacer(modifier = Modifier.height(16.dp))
 
-            Text(
-                text = "Opción seleccionada: ${selectedOption.value}",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.primary
-            )
         }
+
+
     }
+
 }
