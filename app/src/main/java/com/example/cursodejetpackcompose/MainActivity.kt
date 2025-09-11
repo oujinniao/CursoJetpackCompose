6c112081251ec0cabc920a3077978e5ab3421b07
lenovo <grajales1725@gmail.com>
2025-09-11 17:41:35 -0300

    version de JPC usando la funcion ShowDialog

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -5,25 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,9 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cursodejetpackcompose.ui.theme.CursoDeJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,94 +31,82 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CursoDeJetpackComposeTheme {
-
-               DropDownMenuExample()
+                AlertDialogExample()
             }
         }
     }
 
     @Composable
-    fun DropDownMenuExample() {
-        var expanded by remember { mutableStateOf(false) }
-        var selectedOption by remember { mutableStateOf<Pair<String, ImageVector>?>(null) }
-        val options = listOf(
-            "Inicio" to Icons.Default.Home,
-            "Favoritos" to Icons.Default.Check,
-            "Cerrar Sesión" to Icons.Default.Close,
-            "Configuración" to Icons.Default.Settings,
-            "Acerca de" to Icons.Default.Check,
-            "Ayuda" to Icons.Default.Call
-        )
+    fun AlertDialogExample() {
+        var showDialog by remember { mutableStateOf(false) }
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 100.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            Box(
-
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center,
-
-                ) {
-                Button(
-                    onClick = { expanded = true },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        selectedOption?.second?.let {
-                            Icon(
-                                imageVector = selectedOption!!.second,
-                                contentDescription = selectedOption!!.first,
-                                modifier = Modifier.size(24.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-
-                        }
-                        Text(
-                            selectedOption?.first ?: " selecciona una opcion"
-                        )
-
-                    }
-
-                }
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                    offset = DpOffset((-60).dp, 0.dp)
-                ) {
-                    options.forEach { (text, icon) ->
-                        DropdownMenuItem(
-                            text = {
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    Icon(
-                                        imageVector = icon,
-                                        contentDescription = text,
-                                        modifier = Modifier.size(24.dp)
-                                    )
-                                    Spacer(modifier = Modifier.width(8.dp))
-                                    Text(text = text)
-                                }
-                            },
-                            onClick = {
-                                selectedOption = text to icon
-                                expanded = false
-                            }
-                        )
-                    }
-
-                }
-
+            Button(
+                onClick = { showDialog = true },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Mostrar diálogo")
             }
+        }
 
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.alerta),
+                        tint = Color.Unspecified,
+                        contentDescription = "!!!Alerta!!"
+                    )
+                },
+
+                title = {
+                    Text(
+                        text = "¿estas seguro?"
+                    )
+                },
+                text = {
+                    Text(
+                        text = "Esta accion no se puede cancelar.\n ¿desea continuar?",
+                        color = Color.Black,
+                        modifier = Modifier.padding(16.dp),
+                       maxLines = 2,
+                        fontSize = 12.sp,
+
+                    )
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog = false
+                        }
+
+                    ) {
+                        Text("Aceptar")
+                    }
+
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog = false
+                        }
+                    ) {
+                        Text("Cancelar")
+                    }
+                }
+
+            )
         }
     }
 }
+
+
+
+
 
 
 
