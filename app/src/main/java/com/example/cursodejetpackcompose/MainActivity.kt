885567e9b9eb7749dc88c6d1fe7a64d180f7c474
lenovo <grajales1725@gmail.com>
2025-09-16 10:57:57 -0300

    version de JPC usando la funcion RangeSlider

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
+import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CursoDeJetpackComposeTheme {
-            SliderExample()
+            RangeSliderExample()
 
             }
         }
@@ -41,36 +41,47 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    fun SliderExample(){
-        var slaiderValue by remember { mutableStateOf(50f) }
+    fun RangeSliderExample() {
+        var sliderPosition by remember { mutableStateOf(0f..300f) }
 
-        Column (
-            modifier = Modifier.fillMaxSize()
-            .padding(24.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+
         ){
-
             Text(
-                text = "Slider Example: ${slaiderValue.toInt()}",
-                style = MaterialTheme.typography.titleMedium
-
+                text = "Rango de precios",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
             )
             Spacer(modifier = Modifier.height(16.dp))
 
-            Slider(
-                value = slaiderValue,
-                onValueChange = {slaiderValue = it},
-
-                valueRange = 0f..100f,
-                steps = 3,
-                onValueChangeFinished = {
-                    println("El valor final es: $slaiderValue")
-                },
-                colors = SliderDefaults.colors(activeTrackColor = MaterialTheme.colorScheme.primary,
-                    inactiveTrackColor = MaterialTheme.colorScheme.surfaceVariant,
-                    thumbColor = MaterialTheme.colorScheme.primary)
+            Text(
+                text = "Desde: ${sliderPosition.start.toInt()}us$- ${sliderPosition.endInclusive.toInt()}us$",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = 8.dp)
             )
+            RangeSlider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                valueRange = 0f..500f,
+                steps = 5,
+                onValueChangeFinished = {
+                    println("RangeSlider: $sliderPosition.start - ${sliderPosition.endInclusive}")
+                    // launch some business logic update with the state you hold
+                    // viewModel.updateSelectedSliderValue(sliderPosition)
+                },
+                colors = SliderDefaults.colors(
+                    activeTrackColor = MaterialTheme.colorScheme.primary,
+                    inactiveTrackColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f),
+                    thumbColor = MaterialTheme.colorScheme.primary
+                )
+            )
+
         }
     }
 
