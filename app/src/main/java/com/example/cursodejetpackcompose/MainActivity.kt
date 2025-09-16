4a117a050c53e45ff6d3e1f8971a9c7e20ce3215
lenovo <grajales1725@gmail.com>
2025-09-16 16:23:26 -0300

    Funcion Scoffold con topBar

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -6,14 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,38 +33,71 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Black
                 ) {
-                ScaffoldScreen()
+                    ScaffoldScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScaffoldScreen(){
+fun ScaffoldScreen() {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Mi Aplicación")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { /*accion de busqueda*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Buscar"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
         containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
-        content = {paddingValues->   //contenido de la pantalla
+        content = { paddingValues ->   //contenido de la pantalla
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues),
                 contentAlignment = Alignment.Center
-
-            ){
+            ) {
                 Text(
                     text = "Contenido de la pantalla",
                     color = MaterialTheme.colorScheme.onPrimary,
                     style = MaterialTheme.typography.headlineLarge,
                     modifier = Modifier.align(Alignment.Center),
-                    )
+                )
             }
         }
     )
-
 }
 
-
-
-
-
+@Preview(showBackground = true)
+@Composable
+fun ScaffoldScreenPreview() {
+    MaterialTheme {
+        ScaffoldScreen()
+    }
+}
