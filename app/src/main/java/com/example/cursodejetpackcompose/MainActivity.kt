57e080f0e7ab104494a9dc7cc595027f46388b02
lenovo <grajales1725@gmail.com>
2025-09-18 07:44:10 -0300

    Funcion NavigationBar

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -24,6 +22,9 @@ import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -32,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -59,6 +61,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ScaffoldScreen() {
     var isExpanded by remember { mutableStateOf(false) }
+
+    val items = listOf("Inicio", "Buscar", "Perfil") // lista de etiquetas
+    val icons = listOf(
+        Icons.Default.Home,
+        Icons.Default.Search,
+        Icons.Default.Person) // lista de iconos
+
+var selectedItem by rememberSaveable { mutableStateOf(0) } // variable que almacena la posicion del item seleccionado
+
 
     Scaffold(
         topBar = {
@@ -101,36 +112,29 @@ fun ScaffoldScreen() {
         },
         bottomBar = {
             // Barra inferior
-            BottomAppBar(
-                actions = {
-                    IconButton(
-                        onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Home,
-                            contentDescription = "Home"
-                        )
-                    }
-                    IconButton(
-                        onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = "Buscar"
-                        )
-                    }
-                    IconButton(
-                        onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Favorite,
-                            contentDescription = "Favoritos"
-                        )
-                    }
-                },
-                tonalElevation = 5.dp,
-                windowInsets = WindowInsets.navigationBars,
+            NavigationBar(
+                modifier=Modifier.fillMaxWidth(),
                 containerColor = MaterialTheme.colorScheme.surface,
-                contentColor = MaterialTheme.colorScheme.onSurface
-            )
-        },
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                tonalElevation = 3.dp,
+                windowInsets = NavigationBarDefaults.windowInsets
+
+            ) {
+                items.forEachIndexed { index, label ->
+
+                    NavigationBarItem(
+                        icon = { Icon(icons[index],
+                            contentDescription = label) },
+                        label = { Text(label) },
+                        alwaysShowLabel = true,
+                        selected = selectedItem == index,
+                        onClick = { selectedItem = index }
+                    )
+                }
+            }
+
+
+            },
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 onClick = {
@@ -165,7 +169,7 @@ fun ScaffoldScreen() {
                 contentAlignment = Alignment.Center
             ) {
                 Text(
-                    text = "Contenido de la pantalla",
+                    text = "Pantalla: ${items[selectedItem]}",
                     color = MaterialTheme.colorScheme.onPrimary,
                     style = MaterialTheme.typography.headlineLarge,
                     modifier = Modifier.align(Alignment.Center),
