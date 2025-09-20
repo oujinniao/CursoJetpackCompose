2b74c8e42d302c39342a2a3d5999094d86555db2
lenovo <grajales1725@gmail.com>
2025-09-20 10:33:34 -0300

    Aplicacion usando la función ScrollTabRow

--- app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/cursodejetpackcompose/MainActivity.kt
@@ -3,24 +3,16 @@ package com.example.cursodejetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
@@ -43,94 +35,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Black
                 ) {
-                    TabRowExample()
+                    ScrollableTabRowExample()
                 }
             }
         }
     }
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun TabRowExample(){
-        val tabTitles = listOf("Inicio", "Favoritos", "Perfil")
+    fun ScrollableTabRowExample() {
+        val tabTitles = listOf("Inicio", "Favoritos", "Perfil","Configuración","Acerca de","Ayuda")
         var selectedTabIndex by remember { mutableIntStateOf(0) }
-        val interactionSource = remember {
-            List(tabTitles.size) { MutableInteractionSource() }
-
-             }
-
-        val tabIcons = listOf(
-            Icons.Default.Home,
-            Icons.Default.Favorite,
-            Icons.Default.Person
-        )
 
         Scaffold (
             topBar = {
-                TabRow(
+                ScrollableTabRow(
                     selectedTabIndex = selectedTabIndex,
                     modifier = Modifier.statusBarsPadding(),
-                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    containerColor = Color.Black,
+                    contentColor = Color.White,
+                    edgePadding = 12.dp,
                     indicator = { tabPositions ->
                         TabRowDefaults.SecondaryIndicator(
-                            Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex])
-                                .height(3.dp),
-                            color = MaterialTheme.colorScheme.primary
+                            Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                            color = Color.White
+                        )
 
-                        )
-                    },
-                    divider = {
-                        HorizontalDivider(
-                            color = MaterialTheme.colorScheme.outlineVariant,
-                            thickness = 1.dp
-                        )
                     }
+
                 ) {
                     tabTitles.forEachIndexed { index, title ->
-                        val eneable = index !=1  //con este codigo bloqueamos la pestaña de favoritos
                         Tab(
                             selected = selectedTabIndex == index,
-                            onClick = {
-                                if (eneable)
-                                    selectedTabIndex = index
-
-                            },
-                                //selectedTabIndex = index },
-                            modifier = Modifier.padding(horizontal = 4.dp),
-                            enabled = eneable,
+                            onClick = { selectedTabIndex = index },
                             text = {
                                 Text(
                                     text = title,
-                                    style = MaterialTheme.typography.labelLarge,
-                                    fontSize = 16.sp
-
+                                style = MaterialTheme.typography.labelLarge
                                 )
-                            },
-                            icon = {
-                                Icon(
-                                    imageVector = tabIcons[index],
-                                    contentDescription = title
-                                )
-                            },
-                            selectedContentColor = MaterialTheme.colorScheme.primary,
-                            unselectedContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                            interactionSource = interactionSource[index]
+                            }
                         )
                     }
                 }
             }
-        ){ paddingValues ->
-            Column (
+
+        ){paddingValues ->
+            Column(
                 modifier = Modifier
-                    .fillMaxSize()
                     .padding(paddingValues)
                     .padding(16.dp)
             ){
-                when (selectedTabIndex) {
-                    0 -> Text("pantalla de Inicio", style = MaterialTheme.typography.bodyLarge)
-                    1 -> Text("pantalla Favoritos", style = MaterialTheme.typography.bodyLarge)
-                    2 -> Text(" Pantalla Perfil", style = MaterialTheme.typography.bodyLarge)
-                }
+                Text(
+                    text = "Contenido de la sección ${tabTitles[selectedTabIndex]}",
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontSize = 16.sp)
             }
         }
 
